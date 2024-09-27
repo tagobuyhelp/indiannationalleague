eb70351ac661368d8809a1cd13bed53e18d321cb
tagobuyhelp <tagobuyhelp@gmail.com>
2024-09-27 17:34:15 +0530

    update with test api

--- Server/src/app.js
+++ Server/src/app.js
@@ -11,8 +11,33 @@ import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
 
-app.use(cors());
 
+// Define allowed origins
+const allowedOrigins = [
+    'http://157.173.216.224',
+    'http://127.0.0.1:5500',
+    'http://localhost:4055',
+    'https://secure.indiannationalleague.party',
+    'https://api.indiannationalleague.party/',
+    '62.72.59.121',
+];
+
+// CORS configuration
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log('Request Origin:', origin);
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            console.log('Origin not allowed:', origin);
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static("public"));
